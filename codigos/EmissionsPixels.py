e8cef2b74435f20ddd1bdb3b29a90d9c1613d819
Marcos1292 <m.h.perrude@gmail.com>
2025-05-21 17:34:05 -0300

    Update EmissionsPixels.py

--- codigos/EmissionsPixels.py
+++ codigos/EmissionsPixels.py
@@ -14,13 +14,13 @@ from emissionsGrid import EmssionsGrid
 
 
 #
-def EmissionsPixelsWoodCoal(Tam_pixel, Combustivel, woodEmission, coalEmission, 
-                       totalEmission, gridGerado, DataPath, OutPath, uf, br_uf, 
+def EmissionsPixelsWoodCoal(Tam_pixel, Combustivel, woodEmission, coalEmission
+                       , gridGerado, DataPath, OutPath, uf, br_uf, 
                        poluentesWoodCoal, setores):
 
 
     # Padroniza CD_SETOR
-    for df in [woodEmission, coalEmission, totalEmission]:
+    for df in [woodEmission, coalEmission]:
         df['CD_SETOR'] = df['CD_SETOR'].astype(str)
 
     # Processa os estados dentro do grid
@@ -40,8 +40,6 @@ def EmissionsPixelsWoodCoal(Tam_pixel, Combustivel, woodEmission, coalEmission,
         emissoes_uf = pd.merge(woodEmission, gdf_uf[['CD_SETOR', 'geometry']], on='CD_SETOR', how='right')
     elif Combustivel == "Carvao":
         emissoes_uf = pd.merge(coalEmission, gdf_uf[['CD_SETOR', 'geometry']], on='CD_SETOR', how='right')
-    elif Combustivel == "All":
-        emissoes_uf = pd.merge(totalEmission, gdf_uf[['CD_SETOR', 'geometry']], on='CD_SETOR', how='right')
 
     emissoes_uf = gpd.GeoDataFrame(emissoes_uf, geometry='geometry', crs=gdf_uf.crs)
     setores_brasil.append(emissoes_uf)
@@ -55,7 +53,7 @@ def EmissionsPixelsWoodCoal(Tam_pixel, Combustivel, woodEmission, coalEmission,
 
 
 #Gerar Grid de emissoes de GLP
-def EmissionsPixelsGLP (combDf ,br_mun, gridGerado):
+def EmissionsPixelsGLP (combDf ,br_mun, gridGerado, poluentesGLP):
     br_mun['CD_MUN'] = br_mun['CD_MUN'].astype(int)#Shape com geometria
     combDf['CODIGO IBGE'] = combDf['CODIGO IBGE'].astype(int)
     combDf.rename(columns={'CODIGO IBGE': 'CD_MUN'}, inplace = True)
@@ -70,9 +68,8 @@ def EmissionsPixelsGLP (combDf ,br_mun, gridGerado):
     geoCombDf['CD_MUN'] = geoCombDf['CD_MUN'].astype(object)
     geoCombDf = gpd.GeoDataFrame(geoCombDf, geometry='geometry')
 
-    poluentes = ['PM', 'SO2', 'Nox', 'N2O', 'CO2', 'CO', 'CH4']
     
-    emiGrid = EmssionsGrid(geoCombDf, gridGerado, poluentes)
+    emiGrid = EmssionsGrid(geoCombDf, gridGerado, poluentesGLP)
 
     return emiGrid
 
@@ -96,35 +93,54 @@ def cellTimeZone(xx,yy):
                           np.shape(xx))[1])
     return ltcGrid
 
+def geoGrid4mat(combustivel, emiGrid,gridMat4D,poluentes,DataPath):
+    temporalFactorHist = pd.read_csv(DataPath + '\\fatdesEPE.csv', index_col = [0])
+    
+    #Localiza o historico de consumo
+    temporalFactorCombustivel = temporalFactorHist.loc[combustivel].reset_index()
+    
+    #Loop para cada poluente
+    for ii, pol in enumerate(poluentes):
+        gridMat = np.reshape(emiGrid[pol].fillna(0),
+                             (np.shape(np.unique(emiGrid.lon))[0],
+                              np.shape(np.unique(emiGrid.lat))[0])).transpose()
+   
+        #Loop para cada Ano
+        for jj in range(gridMat4D.shape[1]):
+            gridMat4D[ii,jj,:] = gridMat4D[ii,jj,:,:]  + gridMat * temporalFactorCombustivel[combustivel].iloc[jj]
+                
+    return gridMat4D
 
-def geoGrid2mat(emiGrid,gridMat4D,poluentes,uf,ltcGrid,DataPath):
+def geoGrid5mat(gridMat4D,gridMat5D,poluentes,uf,ltcGrid,DataPath):
     temporalFactor = pd.read_csv(DataPath + '\\fatdes.csv')
     
     #Localiza a desagregação temporal no csv de acordo com UF
     temporalFactorUF = temporalFactor[temporalFactor['UF']==uf]
     # Loop para cara poluente
     for ii, pol in enumerate(poluentes):
-        gridMat = np.reshape(emiGrid[pol].fillna(0),
-                             (np.shape(np.unique(emiGrid.lon))[0],
-                              np.shape(np.unique(emiGrid.lat))[0])).transpose()
      # apara cad apoluente e mes, agrupar pelo fuso horário
      # apara cad apoluente e mes, agrupar pelo fuso horário
         # for jj in range(0,12):
         #     gridMat4D[ii, jj, :, :] == gridMat4D[ii, jj, :, :] + gridMat * pesos[jj]
             #gridMat4D[ii,jj,:] =gridMat4D[ii,jj,:,:]  + gridMat*temporalFactorUF['Peso'].reset_index().iloc[jj].Peso
             # gridMat4D[ii,jj,idx]=gridMat4D[ii,jj,idx]+ gridMat[idx]* np.roll(temporalFactorUF['Peso'],
-            #                                                                          int(utcoff))[jj]
-        for jj in range(12):
-            gridMat4D[ii,jj,:] =gridMat4D[ii,jj,:,:]  + gridMat*temporalFactorUF['Peso'].reset_index().iloc[jj].Peso 
-        # for jj in range(0,12):
-        #     jj=0
-        #     utcoffs = np.unique(ltcGrid)
-        #     for utcoff in utcoffs:
-        #         utcoff = utcoffs[0]
-        #         idx = ltcGrid==utcoff
-        #         gridMat4D[ii,jj,idx] =gridMat4D[ii,jj,idx]  + gridMat[idx]*temporalFactorUF['Peso'].iloc[jj]
-        #         # gridMat4D[ii,jj,idx]=gridMat4D[ii,jj,idx]+ gridMat[idx]* np.roll(temporalFactorUF['Peso'],
-        #         #                                                                          int(utcoff))[jj]
-    #A = gridMat4D[0,0,:,:]              
-    return gridMat4D
+            #int(utcoff))[jj]
+        
+        # Loop para cada Ano    
+        for yy in range(gridMat4D.shape[1]): 
+            
+            # Loop para cada mes
+            for jj in range(12):
+                gridMat5D[ii,yy,jj,:,:] = gridMat4D[ii,yy,:,:]  +  gridMat4D[ii,yy,:,:] * temporalFactorUF['Peso'].reset_index().iloc[jj].Peso 
+            # for jj in range(0,12):
+            #     jj=0
+            #     utcoffs = np.unique(ltcGrid)
+            #     for utcoff in utcoffs:
+            #         utcoff = utcoffs[0]
+            #         idx = ltcGrid==utcoff
+            #         gridMat4D[ii,jj,idx] =gridMat4D[ii,jj,idx]  + gridMat[idx]*temporalFactorUF['Peso'].iloc[jj]
+            #         # gridMat4D[ii,jj,idx]=gridMat4D[ii,jj,idx]+ gridMat[idx]* np.roll(temporalFactorUF['Peso'],
+            #         #                                                                          int(utcoff))[jj]            
+            
+    return gridMat5D
 
